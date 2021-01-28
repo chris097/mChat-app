287b8ddd63c3b8ef3ad3e59879aa5d826a3bca62
chris097 <chrisfidel.international@gmail.com>
2021-01-29 00:02:56 +0100

    add tools for the project

--- src/Component/User/UserStyled.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const UserWrapper = styled.div`
-    width: 300px;
-    height: 100%;
-    position: fixed;
-   left: 100px;
-   background-color: #f5f5f5;
-`;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './Component/App';
-import "./Component/Reset.css";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
